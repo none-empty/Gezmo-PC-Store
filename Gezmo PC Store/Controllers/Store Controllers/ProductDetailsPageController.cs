c6c547d0bd2694399996589b63bdf7a4383ae16b
Omar <oiamer4778@gmail.com>
2025-02-12 22:06:12 +0200

    feat: addding ProductDetails page

---
+++ Gezmo PC Store/Controllers/Store Controllers/ProductDetailsPageController.cs
@@ -0,0 +1,16 @@
+﻿using Gezmo_PC_Store.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gezmo_PC_Store.Controllers.Store_Controllers;
+
+public class ProductDetailsPageController:BaseController
+{
+    public ProductDetailsPageController(IGlobalsHelper globalsHelper, IDataProvider dataProvider) : base(globalsHelper, dataProvider)
+    {
+    }
+
+    public IActionResult ProductDetails()
+    {
+        return View();
+    }
+}
