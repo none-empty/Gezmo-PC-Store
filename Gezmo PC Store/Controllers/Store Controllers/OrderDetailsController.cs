d7396d3f5d7d8a50dc27bae79d2748b7a6c9f941
Omar <oiamer4778@gmail.com>
2025-02-16 18:32:54 +0200

    feat: adding the Order details page

---
+++ Gezmo PC Store/Controllers/Store Controllers/OrderDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using Gezmo_PC_Store.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gezmo_PC_Store.Controllers.Store_Controllers;
+
+public class OrderDetailsController:BaseController
+{
+    public OrderDetailsController(IGlobalsHelper globalsHelper, IDataProvider dataProvider) : base(globalsHelper, dataProvider)
+    {
+    }
+
+    public IActionResult OrderDetails()
+    {
+        return View();
+    }
+}
