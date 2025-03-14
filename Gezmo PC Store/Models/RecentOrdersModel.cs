4ce94518c290946dc2179778f34a3c43c3dd5779
Omar <oiamer4778@gmail.com>
2025-03-14 15:42:20 +0200

    feat: adding RecentOrders Models.

---
+++ Gezmo PC Store/Models/RecentOrdersModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Gezmo_PC_Store.Models;
+
+public class RecentOrdersModel
+{
+   public List<UserOrder> userOrders = Enumerable.Empty<UserOrder>().ToList();
+    
+}
---
+++ Gezmo PC Store/Models/UserOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Gezmo_PC_Store.Models;
+
+public class UserOrder
+{
+    public long OrderId { get; set; }
+    public int TotalPrice { get; set; }
+
+    public DateOnly OrderDate { get; set; }
+    public string? Status { get; set; }
+}
