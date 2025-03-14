0820d8fd1df0ecd8009e39b5d57d49ba00d36d51
Omar <oiamer4778@gmail.com>
2025-03-15 00:57:16 +0200

    feat: adding confirm order functionality.

--- Gezmo PC Store/Controllers/Store Controllers/OrderDetailsController.cs
+++ Gezmo PC Store/Controllers/Store Controllers/OrderDetailsController.cs
@@ -1,16 +1,32 @@
-﻿using Gezmo_PC_Store.Services;
+﻿using Gezmo_PC_Store.Models;
+using Gezmo_PC_Store.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gezmo_PC_Store.Controllers.Store_Controllers;
 
 public class OrderDetailsController:BaseController
 {
-    public OrderDetailsController(IGlobalsHelper globalsHelper, IDataProvider dataProvider) : base(globalsHelper, dataProvider)
+    private readonly IOrdersHandler _ordersHandler;
+    public OrderDetailsController(IGlobalsHelper globalsHelper, IDataProvider dataProvider,IOrdersHandler ordersHandler) : base(globalsHelper, dataProvider)
     {
+        _ordersHandler = ordersHandler;
     }
 
     public IActionResult OrderDetails()
     {
         return View();
+    }
+
+    public IActionResult ConfirmOrder()
+    {
+        var glb = _globalsHelper.FetchGlobals(HttpContext);
+        if (!glb.LoggedIn)
+        {
+            return RedirectToAction("Login", "LoginRegister");
+        }
+        _ordersHandler.InsertOrder(glb.cart, glb.User!.UserId);
+        glb.cart = new Cart();
+        _globalsHelper.SetGlobals(HttpContext,glb);
+        return RedirectToAction("RecentOrders", "RecentOrders");
     }
 }
