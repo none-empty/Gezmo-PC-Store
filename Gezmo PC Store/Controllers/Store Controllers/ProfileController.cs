349b81db8eec7cd11923ef1a2c868a3998cde4dd
Omar <oiamer4778@gmail.com>
2025-03-14 15:40:18 +0200

    refactor: adding profie page controller.
    note: this change was supposed to be commited in the previous commit but the file mistakenly was not staged.

---
+++ Gezmo PC Store/Controllers/Store Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Gezmo_PC_Store.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gezmo_PC_Store.Controllers.Store_Controllers;
+
+public class ProfileController:BaseController
+{
+    public ProfileController(IGlobalsHelper globalsHelper, IDataProvider dataProvider) : base(globalsHelper, dataProvider)
+    {
+    }
+    public IActionResult Profile()
+    {
+        return View();
+    }
+}
