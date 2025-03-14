c90e4d17b70809b0abac7691b11cb87e3e6def7e
Omar <oiamer4778@gmail.com>
2025-03-14 15:36:16 +0200

    refactor: putting all concrete classes at services in one sub directory.

---
+++ Gezmo PC Store/Services/Concrete Implementations/OrdersHandler.cs
@@ -0,0 +1,27 @@
+﻿using Gezmo_PC_Store.DataBaseModels;
+using Gezmo_PC_Store.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gezmo_PC_Store.Services;
+
+public class OrdersHandler: IOrdersHandler
+{
+private readonly StoreDbContext _context;
+    public OrdersHandler(StoreDbContext context)
+    {
+       _context = context; 
+    }
+    public async Task<List<UserOrder>> GetUserOrders(int userId)
+    {
+        var userOrders = await _context.Orders.Where(e=>e.UserId == userId)
+            .Select(e => new UserOrder
+        {
+            OrderId = e.OrderId,
+            OrderDate = e.OrderDate,
+            Status = e.Status,
+            TotalPrice = e.TotalPrice
+        }).ToListAsync();
+        
+        return userOrders ;
+    }
+}
