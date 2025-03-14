cd9d062735d380c4e86a344d0429710c93e10f02
Omar <oiamer4778@gmail.com>
2025-03-15 00:56:51 +0200

    feat:the implementation of the two singletons.

---
+++ Gezmo PC Store/Services/Concrete Implementations/NextOrderDetailID.cs
@@ -0,0 +1,36 @@
+﻿using Gezmo_PC_Store.DataBaseModels;
+
+namespace Gezmo_PC_Store.Services;
+
+public class NextOrderDetailID
+{
+    private static readonly object _lockObject = new object();
+    private static long _nextOrderDetailID;
+    public NextOrderDetailID(IServiceProvider services)
+    {
+        InitializeNextUserID(services);
+    }
+
+    private void InitializeNextUserID(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            _nextOrderDetailID = context.OrderDetails.Any()? context.OrderDetails.Max(e=>e.OrderDetailId):1;
+            
+        }
+        
+    }
+
+    public long GetNextOrderDetailId()
+    {
+        long current = 0;
+        lock (_lockObject)
+        {
+            current = _nextOrderDetailID;
+            _nextOrderDetailID++;
+        }
+        
+        return current;
+    }
+}
---
+++ Gezmo PC Store/Services/Concrete Implementations/NextOrderID.cs
@@ -0,0 +1,35 @@
+﻿using Gezmo_PC_Store.DataBaseModels;
+
+namespace Gezmo_PC_Store.Services;
+
+public class NextOrderID
+{
+    private static readonly object _lockObject = new object();
+    private static long _nextOrderID;
+    public NextOrderID(IServiceProvider services)
+    {
+        InitializeNextOrderID(services);
+    }
+
+    private void InitializeNextOrderID(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            _nextOrderID = context.Orders.Any()?context.Orders.Max(e=>e.OrderId):1;
+        }
+        
+    }
+
+    public long GetNextOrderId()
+    {
+        long current = 0;
+        lock (_lockObject)
+        {
+            current = _nextOrderID;
+            _nextOrderID++;
+        }
+        
+        return current;
+    }
+}
