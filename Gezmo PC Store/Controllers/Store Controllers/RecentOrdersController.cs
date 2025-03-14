e617e9c21159b8b343686c9376a0650886ad353f
Omar <oiamer4778@gmail.com>
2025-03-14 15:41:50 +0200

    feat: adding RecentOrders page and it's controller.

---
+++ Gezmo PC Store/Controllers/Store Controllers/RecentOrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Gezmo_PC_Store.Models;
+using Gezmo_PC_Store.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gezmo_PC_Store.Controllers.Store_Controllers;
+
+public class RecentOrdersController:BaseController
+{
+    private readonly IOrdersHandler _ordersHandler;
+    public RecentOrdersController(IGlobalsHelper globalsHelper, IDataProvider dataProvider,IOrdersHandler ordersHandler) : base(globalsHelper, dataProvider)
+    {
+        _ordersHandler = ordersHandler;
+    }
+
+    public async Task<IActionResult> RecentOrders()
+    {
+        var userid = _globalsHelper
+            .FetchGlobals(HttpContext)
+            .User!
+            .UserId;
+        var model = new RecentOrdersModel
+        {
+            userOrders = await _ordersHandler.GetUserOrders(userid)
+        };
+        return View(model);
+    }
+}
